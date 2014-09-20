a00cd97de644334b141d06592da8b7443e136d84
Aikar <aikar@aikar.co>
2014-09-20 02:06:48 -0400

    fix error in UBPaste

--- lib/UBPasteService.php
+++ lib/UBPasteService.php
@@ -15,7 +15,7 @@
 class UBPasteService extends StorageService {
 
 	public function get($id) {
-		$data = $this->requestUrl('http://paste.ubuntu.com/' . intval($_REQUEST['url']));
+		$data = $this->requestUrl('http://paste.ubuntu.com/' . intval($id));
 		if (preg_match_all('/<pre>(.*?)<\/pre>/msi', $data, $m)) {
 			return Util::sanitize($m[1][1]);
 		}
