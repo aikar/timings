9214f3f8662f02c653b89f4a56014d8c5cbfd8be
Aikar <aikar@aikar.co>
2014-09-27 22:08:36 -0400

    Make Cache support GZIP content

--- lib/Cache.php
+++ lib/Cache.php
@@ -12,11 +12,18 @@ class Cache {
 
 	public static function get($key) {
 		$file = self::getFile($key);
-		return file_exists($file) ? trim(file_get_contents($file)) : null;
+		if (file_exists("$file.gz")) {
+			return trim(gzdecode(file_get_contents("$file.gz")));
+		} else if (file_exists($file)) {
+			$data = trim(file_get_contents($file));
+			self::put($key, $data);
+			//unlink($file); // Disabled for now while legacy code is the primary code
+		}
+		return null;
 	}
 	public static function put($key, $data) {
 		if (strlen($data) < MAX_CACHE_BYTES) {
-			file_put_contents(self::getFile($key), $data);
+			file_put_contents(self::getFile($key).".gz", gzencode($data));
 		}
 	}
 	private static function getFile($key) {
