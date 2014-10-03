ea219d3c54b6453d7657784c635876b39916015d
Aikar <aikar@aikar.co>
2014-10-02 23:16:56 -0400

    some changes to Cache needed for things

--- lib/Cache.php
+++ lib/Cache.php
@@ -8,8 +8,13 @@
  */
 
 class Cache {
-	private static $base = '/tmp/timings_';
+	public static $base = '/tmp/timings_';
 
+	/**
+	 * Get with arbitrary string key that will be hashed.
+	 * @param $key
+	 * @return null|string
+	 */
 	public static function get($key) {
 		$file = self::getFile($key);
 		if (file_exists("$file.gz")) {
@@ -21,8 +26,41 @@ class Cache {
 		}
 		return null;
 	}
+
+	/**
+	 * Get if you know the direct hash to look up.
+	 * @param $key
+	 * @return null|string
+	 */
+	public static function getHash($key) {
+		$file = self::$base."$key.gz";
+		if (file_exists($file)) {
+			$data = trim(file_get_contents($file));
+			self::put($key, $data);
+			unlink($file);
+		}
+		return null;
+	}
+
+
+	/**
+	 * Use with arbitrary string key that will be hashed.
+	 * @param $key
+	 * @param $data
+	 */
 	public static function put($key, $data) {
 		$file = self::getFile($key).".gz";
+		$data = gzencode($data);
+		file_put_contents($file, $data);
+	}
+
+	/**
+	 * Use if you already have a hash to store with.
+	 * @param $key
+	 * @param $data
+	 */
+	public static function putHash($key, $data) {
+		$file = self::$base."$key.gz";
 		$data = gzencode($data);
 		file_put_contents($file, $data);
 	}
