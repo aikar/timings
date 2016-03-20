f99df449154e5410ac76e4bca0af2681aa1882bb
Aikar <aikar@aikar.co>
2016-03-19 23:37:08 -0400

    chunk improvements

--- template/sections/chunks.php
+++ template/sections/chunks.php
@@ -33,14 +33,21 @@ $sortChunk = function($c1, $c2) use($template, $timingsData) {
         return $c1c < $c2c ? 1 : -1;
     }
 };
+echo "<br /><h2>NOTICE: Counts are NOT EXACT!!!</h2>They are a representation of number of times seen 
+within this selected history window. They will be much higher than seen in game. It is intended 
+to help you identify where the most TE/E's are.<br /><br />";
+
 foreach ($template->areaMap as $worldName => $chunks) {
     usort($chunks, $sortChunk);
     echo "<h3>$worldName</h3>";
     foreach ($chunks as $area) {
         echo $area['world'] . ':' . ($area['x']) . ',' . ($area['z']). "<br />\n";
-        echo "Totals: {$area['ec']} Entities - {$area['tec']} Tile Entities<br /><br />";
+        echo "Totals: {$area['ec']} Entities - {$area['tec']} Tile Entities<br />";
+        echo "Area Seen: {$area['count']} times<br/><br/>";
         echo '<div class="chunk-row full-timing-row">';
-        foreach ($area['te'] + $area['e'] as $k => $v)  {
+        $stuff = $area['te'] + $area['e'];
+        arsort($stuff);
+        foreach ($stuff as $k => $v)  {
             echo "<span class='indent full-child'>$k: $v</span>";
         }
         echo '</div><br />';
