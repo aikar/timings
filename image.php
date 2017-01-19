73d3bfb292232f7fd3fda497660d48bcf06bd6b1
Thomas Edwards <tmfksoft@gmail.com>
2017-01-19 23:59:09 +0000

    Theme changes (#26)

--- image.php
+++ image.php
@@ -1,11 +1,12 @@
 <?php
 if (empty($_GET['id'])) {
-	die;
-}
-$id = basename(preg_replace('/[^0-9a-f]/', '', $_GET['id']));
-$file = "/tmp/timingsimg_" . basename($_GET['id']);
-if (!file_exists($file)) {
 	$file = "static/img/unknown_server.png";
+} else {
+	$id = basename(preg_replace('/[^0-9a-f]/', '', $_GET['id']));
+	$file = "/tmp/timingsimg_" . basename($_GET['id']);
+	if (!file_exists($file)) {
+		$file = "static/img/unknown_server.png";
+	}
 }
 header("Content-Type: image/png");
 readfile($file);
