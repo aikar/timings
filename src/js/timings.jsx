d192226c7db5481ea937be44ce8f9baff6ed20fa
Aikar <aikar@aikar.co>
2017-02-19 20:55:19 -0500

    Add Google Analytics

--- src/js/timings.jsx
+++ src/js/timings.jsx
@@ -19,8 +19,24 @@ import ReactDOM from "react-dom";
 import ContentWrapper from "./ui/ContentWrapper";
 import "./globals";
 
+(function(doc,script,el,first){
+  window['GoogleAnalyticsObject'] = 'ga';
+  window['ga'] = window['ga'] || function () {
+      (window['ga'].q = window['ga'].q || []).push(arguments)
+  };
+  window['ga'].l = 1 * new Date();
+  el = doc.createElement(script);
+  first = doc.getElementsByTagName(script)[0];
+  el.async = 1;
+  el.src = 'https://www.google-analytics.com/analytics.js';
+  first.parentNode.insertBefore(el, first)
+})(document, 'script');
+
+ga('create', 'UA-6236119-3', 'auto');
+ga('send', 'pageview');
+
+
 (function() {
 	ReactDOM.render(<ContentWrapper />, document.getElementById("wrapper"));
 	return data.loadData();
 })();
-
